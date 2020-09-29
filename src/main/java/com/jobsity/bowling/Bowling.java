8b1d276bc7aa900d28951fbc02b113c3fc69238f
Jodevan Campelo <jodevan@gmail.com>
2020-09-29 20:08:36 -0300

    Prepare integration tests

--- src/main/java/com/jobsity/bowling/Bowling.java
+++ src/main/java/com/jobsity/bowling/Bowling.java
@@ -4,11 +4,9 @@ import com.jobsity.bowling.game.BowlingGame;
 import com.jobsity.bowling.game.parser.exception.PlayRecordParseException;
 import com.jobsity.bowling.game.state.exception.InvalidGameStateException;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Stream;
+import java.util.Scanner;
 
 /**
  *
@@ -17,48 +15,42 @@ import java.util.stream.Stream;
 public class Bowling {
 
 	public static void main(String args[]) {
-		if (args.length == 0) {
-			System.out.printf("Usage: java %s <filepath>\n",
-					Bowling.class.getCanonicalName());
-			System.exit(0);
-		}
 
-		String fileName = args[0];
-
-		if (!new File(fileName).exists()) {
-			System.out.printf("File not found: %s\n", fileName);
-			System.exit(1);
-		}
-		
-		BowlingGame game = new BowlingGame();
-
-		AtomicInteger lineNumber = new AtomicInteger(0);
-
-		try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
-			stream.forEach(line -> {
-				try {
-					lineNumber.incrementAndGet();
-					game.processPlayRecord(line);
-				} catch (PlayRecordParseException 
-						| InvalidGameStateException e) {
-					System.out.printf("Error when processing line %d: %s\n", 
-							lineNumber.get(), line);
-					e.printStackTrace();
-					System.exit(1);
-				}
-			});
-		} catch (IOException e) {
-			e.printStackTrace();
-			System.exit(1);
-		}
+		String fileName = args.length > 0 ? args[0] : null;
 		
 		try {
-			game.printGame();
-		} catch (InvalidGameStateException e) {
+			processGame(fileName);
+		} catch (IOException | InvalidGameStateException 
+				| PlayRecordParseException e) {
 			e.printStackTrace();
 			System.exit(1);
 		}
-		
 		System.exit(0);
+
+	}
+
+	public static void processGame(String fileName) throws IOException, 
+			InvalidGameStateException, PlayRecordParseException {
+
+		if (fileName == null) {
+			throw new IOException(String.format("Usage: java %s <filepath>\n",
+					Bowling.class.getCanonicalName()));
+		}
+
+		if (!new File(fileName).exists()) {
+			throw new IOException(
+					String.format("File not found: %s\n", fileName));
+		}
+
+		BowlingGame game = new BowlingGame();
+
+		FileInputStream inputStream = new FileInputStream(fileName);
+		Scanner scanner = new Scanner(inputStream);
+		while (scanner.hasNextLine()) {
+			game.processPlayRecord(scanner.nextLine());
+		}
+		scanner.close();
+
+		game.printGame();
 	}
 }
---
+++ src/test/java/com/jobsity/bowling/game/IntegrationTest.java
@@ -0,0 +1,19 @@
+package com.jobsity.bowling.game;
+
+import org.junit.Test;
+import org.junit.internal.runners.JUnit4ClassRunner;
+import org.junit.runner.RunWith;
+
+/**
+ *
+ * @author jodevan
+ */
+@RunWith(JUnit4ClassRunner.class)
+public class IntegrationTest {
+	
+	@Test
+	public void integrationTest() {
+		
+	}
+	
+}
